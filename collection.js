cad15750b1b55ef2f217d267224349fbd45551b8
henrypeters <meethenry09@gmail.com>
2023-04-28 14:09:00 +0100

    feat:add payment platform, payment calculation logic, user satisfaction, sign up and sign in

--- collection.js
+++ collection.js
@@ -1,15 +1,24 @@
 const arrayOfPictures = ["images/choose-image.png", "images/pasta/spaghetti.jpg", "images/pasta/fusilli.webp", "images/pasta/ruote.jpg",
 "images/pasta/macaroni.jpg", "images/pasta/rigate.jpg", "images/pasta/Pappardelle.jpg", "images/pasta/orzo.jpg", "images/pasta/Indomie.jpg",
-"images/pasta/tagliatelle.jpg", "images/pasta/radiatore.jpg", "images/pasta/gnocchi.jpg", "images/pasta/penne1.jpg", "images/local-foods/Jollof_Rice.jpg", 
+"images/pasta/tagliatelle.jpg", "images/pasta/radiatore.jpg", "images/pasta/gnocchi.jpg", "images/pasta/penne1.jpg", 
+
+"images/local-foods/Jollof_Rice.jpg", 
 "images/local-foods/Yam-Porridge.jpg", "images/local-foods/iyan(pounded_yam).JPG", "images/local-foods/pepper_soup.jpg",
 "images/local-foods/boli(roasted_plantain).jpg", "images/local-foods/ukpo_oka.jpg", "images/local-foods/amala.jpg", "images/local-foods/fried-plantain.jpeg",
 "images/local-foods/egusi.jpg", "images/local-foods/ogbonno.jpg", "images/local-foods/efo-riro.jpg", "images/local-foods/abacha.jpg",
-"images/snacks/puff-puff.jpg"];
+
+"images/snacks/hot_dog2.webp", "images/snacks/cookies.jpg", "images/snacks/meat_pie.jpg", "images/snacks/pizza.jpg", "images/snacks/sandwich2.jpg",
+"images/snacks/plantain_chips.jpg", "images/snacks/popcorn.webp", "images/snacks/corn_dog.jpg", "images/snacks/shawarma.webp", "images/snacks/waffles.webp",
+"images/snacks/burger.jpg", "images/snacks/donut.webp", "images/drinks/coca cola.webp", "images/drinks/fanta.jpg", "images/drinks/schweppes.webp", "images/drinks/mirinda.jpg", "images/drinks/sprite.png",
+"images/drinks/big_pic_smoov.jpg", "images/drinks/pepesi.webp", "images/drinks/LIMCA.png", "images/drinks/lacasera.png", "images/drinks/maltina.jpg", 
+"images/drinks/lucozade.jpg", "images/drinks/5alive.jpg"];
 
 const arraryOfFoodName = ["", "Spaghetti", "Fusilli", "Rhoute", "Macaroni", "Rigate", "Pappardelle", "Orzo", "Indomie",
 "Tagliatelle", "Radiatore", "Gnocchi", "Penne", "Jollof Rice", "Asaro (Yam Porridge)", "Iyan (Pounded Yam)", "Nsala (Pepper Soup)", 
 "Boli (Roasted Plantain)", "Ukpo Oka (Corn Pudding)", "Amala", "Plantain And Egg", "Egusi", "Ogbono", "Efo Riro", "Abacha",
-"Puff Puff"];
+"Hot Dog", "Cookies", "Meat Pie", "Pizza", "Sandwich", "Chips", "Popcorn", "Corn Dog", "Shawarma", "Waffles", "Burger", "Donut",
+"Coca Cola", "Fanta", "Schweppes", "Mirinda", "Sprite", "Chapman", "Pepsi", "Limca", "laCasera", "Maltina", "Lucozade",
+"5alive"];
 
 const arrayOfURL = ["", "pasta/spaghetti/spaghetti.html", "pasta/fusilli/fusilli.html", "pasta/rhoute/rhuote.html",
 "pasta/macaroni/macaroni.html", "pasta/rigate/rigate.html", "pasta/pappardelle/pappardelle.html", "pasta/orzo/orzo.html",
